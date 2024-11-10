e5c60071ed0ab4ad825dbd93e47e148bf7e47271
Afonso Anjos <contatoafonso.dev@gmail.com>
2024-11-09 22:11:24 -0300

    limpeza de código.

--- Provider/ConnectionProvider.cs
+++ Provider/ConnectionProvider.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ControleHospital.Provider
+﻿namespace ControleHospital.Provider
 {
     public class ConnectionProvider
     {
